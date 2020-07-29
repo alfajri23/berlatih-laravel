6e4966ef9053fe7324cc7c7aa6ca4bc507c989c4
alfajri23 <feri.alfajri@gmail.com>
2020-07-29 22:46:25 +0700

    belum selesai

---
+++ tentukan-deret-aritmatika.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+function tentukan_deret_aritmatika($arr) {
+// kode di sini
+$benar = true;
+$salah = false;
+ $beda = $arr[1] - $arr[0];
+   for($i=0; $i<count($arr)-1; $i++){
+        if (($arr[$i + 1]-$arr[$i]) != $beda){
+             return $salah;
+        }
+        return $benar;       
+    }
+    
+}
+
+// TEST CASES
+echo tentukan_deret_aritmatika([1, 2, 3, 4, 5, 6]);// true
+echo tentukan_deret_aritmatika([2, 4, 6, 12, 24]);// false
+echo tentukan_deret_aritmatika([2, 4, 6, 8]); //true
+echo tentukan_deret_aritmatika([2, 6, 18, 54]);// false
+echo tentukan_deret_aritmatika([1, 2, 3, 4, 7, 9]);// false
+?>
+</body>
+</html>
