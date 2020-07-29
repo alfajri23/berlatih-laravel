5fbb6a403443c0b19b8664855adcc2d6a8ebb827
alfajri23 <feri.alfajri@gmail.com>
2020-07-29 21:11:41 +0700

    pwd

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -10,14 +10,33 @@
 
 function palindrome_angka($angka) {
   // tulis kode di sini
+  $henti = true;
+
+  while($henti){
+    $hasil="";
+        for($i=strlen($angka)-1;$i>=0;$i--){
+            $hasil .= $angka[$i];
+        }
+    echo $hasil. "<br>";
+
+    if($angka == $hasil){
+        echo $angka;
+        $henti = false;
+    }
+    else{
+        $henti = true;
+        $angka++;
+    }
+    
+  }
 }
 
 // TEST CASES
-echo palindrome_angka(8) // 9
-echo palindrome_angka(10); // 11
-echo palindrome_angka(117); // 121
-echo palindrome_angka(175); // 181
-echo palindrome_angka(1000); // 1001
+palindrome_angka(8) // 9
+//palindrome_angka(10); // 11
+//palindrome_angka(117); // 121
+//palindrome_angka(175); // 181
+//palindrome_angka(1000); // 1001
 
 ?>
 </body>
