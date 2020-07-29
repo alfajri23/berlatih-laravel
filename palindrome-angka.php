0cec733178d3c5f2003f28e9282493d5487e9eb7
alfajri23 <feri.alfajri@gmail.com>
2020-07-29 21:35:17 +0700

    masih binggung

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
@@ -10,29 +11,18 @@
 
 function palindrome_angka($angka) {
   // tulis kode di sini
-  $henti = true;
-
-  while($henti){
     $hasil="";
         for($i=strlen($angka)-1;$i>=0;$i--){
             $hasil .= $angka[$i];
         }
-    echo $hasil. "<br>";
-
-    if($angka == $hasil){
-        echo $angka;
-        $henti = false;
-    }
-    else{
-        $henti = true;
-        $angka++;
-    }
-    
-  }
+    echo $hasil. "<br>";    
+    return $hasil;
 }
 
+
 // TEST CASES
-palindrome_angka(8) // 9
+echo palindrome_angka("81"); // 9
+echo "hallo aku";
 //palindrome_angka(10); // 11
 //palindrome_angka(117); // 121
 //palindrome_angka(175); // 181
