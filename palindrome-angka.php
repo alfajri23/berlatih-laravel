2b0bf7f7bcc10610dae5b8b1354a9dae668ed8ae
alfajri23 <feri.alfajri@gmail.com>
2020-07-29 19:27:04 +0700

    membuat palindrome

---
+++ palindrome-angka.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+function palindrome_angka($angka) {
+  // tulis kode di sini
+}
+
+// TEST CASES
+echo palindrome_angka(8) // 9
+echo palindrome_angka(10); // 11
+echo palindrome_angka(117); // 121
+echo palindrome_angka(175); // 181
+echo palindrome_angka(1000); // 1001
+
+?>
+</body>
+</html>
