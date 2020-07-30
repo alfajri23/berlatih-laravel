df77e5c7dcb8bfb6c4eb18e6126547ba600753ff
alfajri23 <feri.alfajri@gmail.com>
2020-07-30 07:18:49 +0700

    aritmatika done

--- tentukan-deret-aritmatika.php
+++ tentukan-deret-aritmatika.php
@@ -9,19 +9,31 @@
 <?php
 function tentukan_deret_aritmatika($arr) {
 // kode di sini
-$benar = true;
-$salah = false;
+$woi ;
+
  $beda = $arr[1] - $arr[0];
-   for($i=0; $i<count($arr)-1; $i++){
-        if (($arr[$i + 1]-$arr[$i]) != $beda){
-             return $salah;
+   for($i=0; $i<=count($arr)-2; $i++){
+       $cari=0;
+       $cari=$arr[$i + 1]-$arr[$i];
+       
+        if ( $cari != $beda){
+            
+            
+             $woi = "false";
+            break;
         }
-        return $benar;       
+        else{
+            
+            $woi = "true";
+        }
+             
     }
+    return $woi."<br>";
     
 }
 
 // TEST CASES
+
 echo tentukan_deret_aritmatika([1, 2, 3, 4, 5, 6]);// true
 echo tentukan_deret_aritmatika([2, 4, 6, 12, 24]);// false
 echo tentukan_deret_aritmatika([2, 4, 6, 8]); //true
